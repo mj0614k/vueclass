e17190940870299baf1645e573fb86f730fcb317
mj0614k <mj0614k@gmail.com>
2022-10-26 10:46:08 +0900

    firebase1026

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!-- HTML -->
+<template> 
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<!-- script -->
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<!-- style -->
+<style>
+</style>
